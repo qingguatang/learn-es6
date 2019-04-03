657c3cb441445cdd88dd211487b737af563d271a
bencode <bencode@163.com>
2019-04-03 17:44:47 +0800

    update

--- public/scope/index.js
+++ public/scope/index.js
@@ -1,17 +1,8 @@
 initMenu();
 
 function initMenu() {
-  var i;
-  var lis
-  lis = document.querySelectorAll('.menu li');
-
+  var lis = document.querySelectorAll('.menu li');
   for (var i = 0; i < lis.length; i++) {
-    handleEvent(i);
-  }
-
-  function handleEvent(i) {
-    lis[i].addEventListener('click', function() {
-      console.log(i);
-    });
+    console.log(i);
   }
 }
