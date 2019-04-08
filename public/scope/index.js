29cbc75936cbdd874f7fcb107ded96fe8ce458c1
bencode <bencode@163.com>
2019-04-08 17:23:41 +0800

    update

--- public/scope/index.js
+++ public/scope/index.js
@@ -1,4 +1,4 @@
-// initMenu();
+
 
 function initMenu() {
   var lis;
@@ -37,12 +37,37 @@ function inComputer() {
   }
 }
 
-console.log(1, java);
+// console.log(1, java);
 
-for (let i = 0; i < 10; i++) {
-  let j = i + 1;
-  setTimeout(() => {
-    console.log(i, j);
-  }, 10);
+// for (let i = 0; i < 10; i++) {
+//   let j = i + 1;
+//   setTimeout(() => {
+//     console.log(i, j);
+//   }, 10);
+// }
+
+// 
+function minMax(list) {
+  // var min = 0;
+  let min = list[0];
+  let max = list[0];
+  // let current;
+  // for (let i = 0; i < list.length; i++) {
+  for (const current of list) {
+    // ...
+    // const current = list[i];
+    if (min > current) {
+      // 副作用操作
+      min = current;
+    } else if (max < current) {
+      max = current;
+    }
+  }
+  return [min, max];
 }
 
+const [min, max] = minMax([34, 22, 1, 67, 122]);
+console.log(min, max);
+
+const data = minMax([1, 2,5, 11]);
+console.log(data[0], data[1]);
