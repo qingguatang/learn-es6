caaec05fdcccacf5847ebadca04cda474b5c2803
bencode <bencode@163.com>
2019-04-08 13:59:16 +0800

    update

--- public/scope/index.js
+++ public/scope/index.js
@@ -1,4 +1,4 @@
-initMenu();
+// initMenu();
 
 function initMenu() {
   var lis;
@@ -11,4 +11,37 @@ function initMenu() {
     });
   }
   // console.log(i);
+}
+
+
+var java = '一个岛';
+
+// inComputer();
+
+function inComputer() {
+  var java = '一门计算机语言';
+  console.log(2, java);
+
+  var inMyHome = true;
+  if (inMyHome) {
+    let java = '一只猫';
+    console.log(4, java);
+  }
+
+  console.log(2, java);
+
+  inDev();
+  function inDev() {
+    var java = '面向对象编程语言';
+    console.log(3, java);
+  }
+}
+
+// console.log(1, java);
+
+for (let i = 0; i < 10; i++) {
+  let j = i + 1;
+  setTimeout(() => {
+    console.log(i, j);
+  }, 10);
 }
