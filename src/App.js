1d8a19fe7584d8e5092d4f9c24b1db23b14394aa
bencode <bencode@163.com>
2019-04-11 18:27:35 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import 'bulma';
+import './App.scss';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>解构</h1>
-        <div>使数据访问更便捷</div>
       </div>
     );
   }
