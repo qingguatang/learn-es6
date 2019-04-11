e01001a0c619987af86121e07ae53c53554e1fd3
bencode <bencode@163.com>
2019-04-11 16:04:47 +0800

    start c2

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import React, { Component } from 'react';
-import Button from './components/Button';
-import { Tabs, TabPane } from './components/Tabs';
 import 'bulma';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      {/*
-        <div className="buttons">
-          <Button type="primary" onClick={e => alert('es6')}>
-            <span>React</span>
-          </Button>
-          <Button type="primary" size="large">
-            <span>Large</span>
-          </Button>
-          <Button rounded>
-            <span>Rounded</span>
-          </Button>
-          <Button disabled={true} autoFocus>
-            <span>Default Button</span>
-          </Button>
-        </div>
-      */}
-      <Tabs>
-        <TabPane title="Tab1">ES6 Body 1</TabPane>
-        <TabPane title="Tab2">ES6 Body 2</TabPane>
-        <TabPane title="Tab3">Body 3</TabPane>
-        <TabPane title="Tab4">Body 4</TabPane>
-      </Tabs>
+        <h1>解构</h1>
+        <div>使数据访问更便捷</div>
       </div>
     );
   }
