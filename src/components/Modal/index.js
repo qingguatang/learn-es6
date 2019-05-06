bab8a35e2713acd7a9fc8e481afdb841ec4f0f4b
bencode <bencode@163.com>
2019-05-06 22:58:53 +0800

    step1

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -5,7 +5,28 @@ import './style.scss';
 class Modal extends React.Component {
   render() {
     return (
-      <div>Modal</div>
+      <div>
+         <div className="modal" style={{display: 'block'}} tabIndex="-1" role="dialog">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Modal title</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <p>Modal body text goes here.</p>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="modal-backdrop fade show"></div>
+        </div>
     );
   }
 }
