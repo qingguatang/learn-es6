5f9233dcf207e8b338e8d478474824c288daed47
bencode <bencode@163.com>
2019-05-14 13:30:11 +0800

    finish ex1

--- src/pages/c5/Qrcode.js
+++ src/pages/c5/Qrcode.js
@@ -3,7 +3,7 @@ import qrcode from 'qrcode';
 import style from './style.scss';
 
 
-class Qrcode extends React.Component {
+class Qrcode extends React.PureComponent {
   state = {
     imageUrl: null
   }
@@ -21,17 +21,25 @@ class Qrcode extends React.Component {
   }
 
   componentWillUnmount() {
-    //
+    console.log('unmount');
   }
 
   componentDidUpdate(prev) {
-    console.log('update', this.props, this.state);
+    console.log('update');
     if (prev.url !== this.props.url) {
       qrcode.toDataURL(this.props.url).then(imageUrl => {
         this.setState({ imageUrl });
       });
     }
   }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   if (this.props.url !== nextProps.url ||
+  //     this.state.imageUrl !== nextState.imageUrl) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
 
   render() {
     console.log('render');
@@ -45,22 +53,28 @@ class Qrcode extends React.Component {
 
 class QrcodePage extends React.Component {
   state = {
-    url: 'https://www.google.com'
+    url: 'https://www.google.com',
+    other: 0
   }
 
   render() {
-    const { url } = this.state;
-    return (
+    const { url, other } = this.state;
+    console.log('page render');
+    const node =  (
       <div className={style.qrcode}>
+        <button onClick={() => this.setState({ other: (other + 1) % 2 })}>Other</button>
+        { other ?
+            <div name="a" className="other"><input type="checkbox" />1</div> :
+            <div name="b" className="other"><input type="checkbox" />2</div> }
         <div className="field">
           <input type="text" className="form-control" placeholder="URL"
             value={url} onChange={e => this.setState({ url: e.target.value })} />
         </div>
-        <div className="field">
-          <Qrcode url={this.state.url} />
-        </div>
+        <Qrcode url={this.state.url} />
       </div>
     );
+    console.log(node);
+    return node;
   }
 }
 
