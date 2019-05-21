c38cc73a00de4bacb8731fa7c1389dba04d90746
bencode <bencode@163.com>
2019-05-21 18:46:58 +0800

    step1

--- src/pages/Qrcode/index.js
+++ src/pages/Qrcode/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import './style.scss';
+import style from './style.scss';
 
 class QrcodePage extends React.Component {
   render() {
     return (
-      <div>
+      <div className={style.qrcode}>
         <div className="field">
           <input type="text" className="form-control" placeholder="URL" />
         </div>
