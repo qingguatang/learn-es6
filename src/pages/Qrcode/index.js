72b0ad256cb4f283ae763bf4ee16cd5f72a6ee0f
bencode <bencode@163.com>
2019-05-21 21:37:20 +0800

    step3

--- src/pages/Qrcode/index.js
+++ src/pages/Qrcode/index.js
@@ -1,14 +1,45 @@
 import React from 'react';
+import qrcodeUtil from 'qrcode';
 import 'bootstrap/dist/css/bootstrap.css';
 import style from './style.scss';
 
 class QrcodePage extends React.Component {
+  state = {
+    url: 'https://www.google.com'
+  }
+
   render() {
     return (
       <div className={style.qrcode}>
         <div className="field">
           <input type="text" className="form-control" placeholder="URL" />
         </div>
+        <Qrcode url={this.state.url} />
+      </div>
+    );
+  }
+}
+
+class Qrcode extends React.Component {
+  state = {
+    imgUrl: null
+  }
+
+  componentDidMount() {
+    qrcodeUtil.toDataURL(this.props.url)
+      .then(imgUrl => {
+        this.setState({ imgUrl });
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
+  render() {
+    const { imgUrl } = this.state;
+    return (
+      <div>
+        { imgUrl ? <img src={imgUrl} /> : null }
       </div>
     );
   }
